51eba05d674d04588e26325f18dbdd12417af858
taoy3 <taoy3@taoy3.local>
2016-07-24 22:38:51 +0800

    Initial commit

---
+++ app/src/main/java/com/view/taoy3/mylistview/view/MyListView.java
@@ -0,0 +1,19 @@
+package com.view.taoy3.mylistview.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ListView;
+
+/**
+ * Created by taoy3 on 16/7/24.
+ */
+public class MyListView extends ListView{
+    public MyListView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyListView(Context context) {
+        super(context);
+    }
+
+}
